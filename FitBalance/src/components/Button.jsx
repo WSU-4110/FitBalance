60bb9a7b377ab325a9e712ada3927739bc9385a3
ikbalhussa1n <iqbalhussa1n@yahoo.com>
2025-02-11 16:42:39 -0500

    subtle chnages

--- FitBalance/src/components/Button.jsx
+++ FitBalance/src/components/Button.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-const Button = ({ tag, bgCol, textCol, mt }) => {
+const Button = ({ tag, bgCol, textCol, mt, onClick }) => {
   return (
     <button
+      onClick={onClick}
       style={{
         backgroundColor: bgCol,
         color: textCol,
-        marginTop: mt ? `${mt}px` : "0", // Ensure margin-top is applied if passed
+        marginTop: mt ? `${mt}px` : "0",
       }}
-      className="w-40 h-12 text-white font-medium text-xl cursor-pointer rounded-4xl  "
+      className="w-40 h-12 text-white font-medium text-xl cursor-pointer rounded-4xl"
     >
       {tag}
     </button>
